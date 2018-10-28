ee948e7134f2db94e1d28dcd25914a040f4e4756
Luc Berger <luc.richard1994@gmail.com>
2018-10-28 16:16:48 +0000

    Fix issue where the lis were still showing after being removed. I accidentally had the li nested inside of another li, which is what was causing the issue. Also added the little arrow on the left which will only appear if at least 1 li is present.

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const input = document.querySelector('#input');
 const ul = document.querySelector('#list');
-const list = document.querySelector('#list');
+const arrow = document.querySelector('.arrow-down');
 
-list.addEventListener('click', function(e){
+ul.addEventListener('click', function(e){
     const target = e.target;
     // check if the target element that was clicked on is the cross
     if (target.classList.contains('cross')) {
         // if the cross is clicked, remove the parent element i.e. the whole li
         target.parentNode.remove();
+
+        if (!document.querySelector('li')) {
+            arrow.classList.remove('visible');
+        }
     }
 });
 
@@ -15,15 +19,15 @@ input.addEventListener('keypress', function(e){
     // listen out for the 'enter' key and make sure the input isn't empty
     if (e.keyCode === 13 && input.value) {
         const li = document.createElement('li');
-        li.innerHTML = `<li class="item">
-                            <div id="item-check">
-                                <input type="checkbox" class="item-check__box">
-                                <span>${input.value}</span>
-                            </div>
-                            <i class="fas fa-times cross"></i>
-                        </li>`;
+        li.className = 'list-item';
+        li.innerHTML = `<div id="item-check">
+                            <input type="checkbox" class="item-check__box">
+                            <span>${input.value}</span>
+                        </div>
+                        <i class="fas fa-times cross"></i>`;
         ul.appendChild(li);
         // clear the input bar once enter is pressed
         input.value = '';
+        arrow.classList.add('visible');
     }
 });
