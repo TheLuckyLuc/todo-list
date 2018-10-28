1adef666f99efac067ed6d154021b9ce7757182a
Luc Berger <luc.richard1994@gmail.com>
2018-10-28 14:40:48 +0000

    Add ability to remove items when clicking the 'x'

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const input = document.querySelector('#input');
 const ul = document.querySelector('#list');
+const list = document.querySelector('#list');
+
+list.addEventListener('click', function(e){
+    const target = e.target;
+    // check if the target element that was clicked on is the cross
+    if (target.classList.contains('cross')) {
+        // if the cross is clicked, remove the parent element i.e. the whole li
+        target.parentNode.remove();
+    }
+});
 
 input.addEventListener('keypress', function(e){
     // listen out for the 'enter' key and make sure the input isn't empty
@@ -13,6 +23,7 @@ input.addEventListener('keypress', function(e){
                             <i class="fas fa-times cross"></i>
                         </li>`;
         ul.appendChild(li);
+        // clear the input bar once enter is pressed
         input.value = '';
     }
 });
