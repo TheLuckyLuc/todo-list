c90276e6de2eb4a59327ca89b20296c6ccb22921
Luc Berger <luc.richard1994@gmail.com>
2018-10-29 19:31:10 +0000

    Make the footer appear only when an li is present. Also style the footer buttons so the border only appears on hover. Also make the counter skip any checked lis

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#input');
 const ul = document.querySelector('#list');
 const arrow = document.querySelector('.arrow-down');
 const counter = document.querySelector('#counter');
+const footer = document.querySelector('#footer');
 
 ul.addEventListener('click', function(e){
     const target = e.target;
@@ -9,12 +10,15 @@ ul.addEventListener('click', function(e){
     if (target.classList.contains('cross')) {
         // if the cross is clicked, remove the parent element i.e. the whole li
         target.parentNode.remove();
-        countItems();
 
         if (!document.querySelector('li')) {
+            // if no li element is present, remove the below classes
             arrow.classList.remove('visible');
+            footer.classList.remove('visible-footer');
         }
     }
+    // run the countItems function to update the outstanding item counter
+    countItems();
 });
 
 input.addEventListener('keypress', function(e){
@@ -30,6 +34,7 @@ input.addEventListener('keypress', function(e){
         ul.appendChild(li);
         // clear the input bar once enter is pressed
         input.value = '';
+        // make the arrow appear once there is at least 1 li
         arrow.classList.add('visible');
         countItems();
     }
@@ -37,11 +42,21 @@ input.addEventListener('keypress', function(e){
 
 function countItems(){
     if (document.querySelector('li')) {
+        // if an li is present, make the footer visible
+        footer.classList.add('visible-footer');
+        // select any present lis
         const li = document.querySelectorAll('li');
+        // set a counter to start at 0
         let count = 0;
-        li.forEach(function(){
-            count++;
-        });
+        // loop through each li that's present and increment the counter by 1 each time
+        for (item of li) {
+            // check if the checkbox of the current li is checked
+            if (!item.childNodes[0].childNodes[1].checked) {
+                // if it isn't checked, increment the counter by 1
+                count++;
+            }
+        }
+        // update the counter with the outstanding lis once the loop has completed
         return (count === 1) ? counter.textContent = `${count} item left` : counter.textContent = `${count} items left`;
     }
 }
